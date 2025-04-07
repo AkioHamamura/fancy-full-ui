f03df134067bd19c6608a0ce1b27b21328f5e486
Akio Hamamura <73171585+AkioHamamura@users.noreply.github.com>
2025-04-07 10:43:47 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+    const API_URL = "https://2hx4ndw6u0.execute-api.us-east-1.amazonaws.com/v1/weather/"
+    function getWeather(city: string, method: string){
+
+        const url = `${API_URL}${method}?&q=${city}`
+        console.log(url)
+    }
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => {
+            setCount((count) => count + 1)
+            getWeather("63701", "current.json")
+        }
+
+        }>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
